4a1afd9e72be4f84e609d265f216298dc7d851aa
Fnart0 <david.benoit.cardinal@gmail.com>
2026-02-24 17:02:33 -0500

    TP terminé

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -4,7 +4,7 @@ export default class Chef {
   constructor(element) {
     this.element = element;
     this.menu = [];
-    this.container = this.element.querySelector('.js-chef__order');
+    this.container = this.element.querySelector('.js-container');
     this.init();
   }
 
@@ -17,7 +17,7 @@ export default class Chef {
       this.menu.push(instance);
     }
 
-    const buttons = this.element.querySelectorAll('.button-secondary');
+    const buttons = this.element.querySelectorAll('.js-button-secondary');
     for (let i = 0; i < buttons.length; i++) {
       const button = buttons[i];
       button.addEventListener('click', this.sendOrder.bind(this));
@@ -27,13 +27,17 @@ export default class Chef {
   sendOrder() {
     this.container.innerHTML = '';
 
-    const nombrePoutine = 0;
-    if (this.element.classList.contains('is-active')) {
-      nombrePoutine + 1;
+    let nombre = 0;
+    for (let i = 0; i < this.menu.length; i++) {
+      const poutine = this.menu[i];
+
+      if (poutine.selectedType != '') {
+        nombre++;
+      }
     }
 
     const totalPoutine = document.createElement('p');
-    totalPoutine.innerText = `Nombre total de poutine(s): ${nombrePoutine}`;
+    totalPoutine.innerText = `Nombre total de poutine(s): ${nombre}`;
     this.container.appendChild(totalPoutine);
   }
 }
