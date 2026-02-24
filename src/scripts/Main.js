7161d5ee89d1fde1fc684d113cf4804aaf1b2bd5
Fnart0 <david.benoit.cardinal@gmail.com>
2026-02-24 13:39:36 -0500

    L'instance de Chef.js est terminé.

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,20 @@
-import Icons from "./utils/Icons.js";
+import Icons from './utils/Icons.js';
+import Chef from './Chef.js';
 
-// La ligne suivante devrait être au TOUT début du init() du Main
-Icons.load();
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    Icons.load();
+
+    const buttons = document.querySelectorAll('.button');
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      new Chef();
+    }
+  }
+}
+
+new Main();
